362866ff5a3354226b720be043a00f519ac8f8fe
Ab4rdo <fryderyk97@protonmail.com>
2017-12-08 22:21:41 +0100

    working on views.py

--- kwzink/urls.py
+++ kwzink/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'kwzink'
 
 urlpatterns = [
-    path('', views.home),
-    path('contact', views.contact),
-    path('about', views.about),
-    path('form', views.form),
-    path('inks', views.inks),
-    path('inks/ig', views.ig),
-    path('inks/standard', views.standard),
+    url('', views.index, name='index'), # home dir
+#   path('contact', views.contact),
+#   path('about', views.about),
+#   path('form', views.form),
+#   path('inks', views.inks),
+#   path('inks/ig', views.ig),
+#   path('inks/standard', views.standard),
 ]
