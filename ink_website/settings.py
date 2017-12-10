85b5a7ddfb12fc5f63c23db91147e6ae10b70a6f
Fryderyk Penkala <fryderyk97@gmail.com>
2017-12-10 18:38:14 +0100

    Update settings.py

--- ink_website/settings.py
+++ ink_website/settings.py
@@ -15,13 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# MAIL
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'fryderyk97@gmail.com'
-EMAIL_HOST_PASSWORD = 'tnzr ihby gkpe fsek'
-EMAIL_PORT = 587
+
 
 
 # SECURITY WARNING
