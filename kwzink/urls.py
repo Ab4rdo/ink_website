23d798c7ea68f9ca3dcfab69806057b8e26a8ba3
Ab4rdo <fryderyk97@protonmail.com>
2017-12-10 11:28:11 +0100

    update urls to django 2.0 standard

--- kwzink/urls.py
+++ kwzink/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = 'kwzink'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'), # home dir
-#   path('contact', views.contact),
-#   path('about', views.about),
+    path('', views.index, name='home'), # home dir
+    path('contact/', views.contact, name='contact'), 
+    path('about/', views.about, name='about'),
 #   path('form', views.form),
 #   path('inks', views.inks),
 #   path('inks/ig', views.ig),
