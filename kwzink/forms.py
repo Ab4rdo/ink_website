7206c1b59608c680c72027069244ec7f276b530b
Ab4rdo <fryderyk97@protonmail.com>
2017-12-10 17:39:57 +0100

    forms.py typo

--- kwzink/forms.py
+++ kwzink/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class ContactForm(forms.Form):
     
-    name_surname = forms.CharFiled(required=True)
+    name_surname = forms.CharField(required=True)
     email = forms.EmailField(required=True)
     subject = forms.CharField(required=True)
     message = forms.CharField(widget=forms.Textarea)
